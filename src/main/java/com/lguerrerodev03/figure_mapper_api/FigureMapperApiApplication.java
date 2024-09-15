a92a773b5e191256b1b688a4ce64a2ee3e6ba61b
Luis Guerrero <lguerrero03developer@gmail.com>
2024-09-15 11:32:49 -0500

    cambios antiguos

--- src/main/java/com/lguerrerodev03/figure_mapper_api/FigureMapperApiApplication.java
+++ src/main/java/com/lguerrerodev03/figure_mapper_api/FigureMapperApiApplication.java
@@ -10,12 +10,4 @@ public class FigureMapperApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FigureMapperApiApplication.class, args);
 	}
-
-	@PostConstruct
-	public void init() {
-		// Verificar si las variables de entorno están disponibles
-		System.out.println("DB URL: " + System.getenv("DB_URL_AZURE"));
-		System.out.println("DB Username: " + System.getenv("DB_USERNAME_AZURE"));
-		System.out.println("JWT Secret Key: " + System.getenv("JWT_SECRET_KEY"));
-	}
 }
